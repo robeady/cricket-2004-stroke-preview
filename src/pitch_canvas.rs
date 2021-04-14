f9986057e867beb980b2e877108d43089dca5633
Rob Eady <robertj.eady@gmail.com>
2021-04-14 04:27:53 +0100

    cleanup

--- src/pitch_canvas.rs
+++ src/pitch_canvas.rs
@@ -2,11 +2,10 @@ use std::cmp::min;
 use std::f64::consts::TAU;
 
 use winapi::{
-    shared::windef::{HBRUSH, HDC, HPEN, RECT},
+    shared::windef::{HBRUSH, HDC, RECT},
     um::{
         wingdi::{
-            CreatePen, CreateSolidBrush, Ellipse, GetStockObject, Pie, SelectObject, NULL_PEN,
-            PS_SOLID, RGB, WHITE_PEN,
+            CreateSolidBrush, Ellipse, GetStockObject, Pie, SelectObject, NULL_PEN, RGB, WHITE_PEN,
         },
         winuser::{FillRect, GetSysColorBrush, COLOR_MENU},
     },
@@ -16,8 +15,7 @@ use crate::strokes::{Stroke, StrokeTiming};
 
 pub struct PitchPainter {
     background: HBRUSH,
-    border: HBRUSH,
-    pen: HPEN,
+
     green: HBRUSH,
     dark_green: HBRUSH,
 
@@ -31,8 +29,6 @@ impl PitchPainter {
             PitchPainter {
                 // FIXME: native-windows-gui sets the wrong background color and we can't change it
                 background: GetSysColorBrush(COLOR_MENU),
-                border: CreateSolidBrush(RGB(100, 100, 255)),
-                pen: CreatePen(PS_SOLID as _, 2, RGB(20, 20, 20)),
                 green: CreateSolidBrush(RGB(0, 190, 0)),
                 dark_green: CreateSolidBrush(RGB(0, 150, 0)),
                 stroke_min: CreateSolidBrush(RGB(250, 100, 50)),
@@ -51,13 +47,14 @@ impl PitchPainter {
             FillRect(hdc, rc, self.background as _);
 
             // pitch
-            let pad = 5;
+
+            let padding = 5;
             SelectObject(hdc, self.green as _);
             SelectObject(hdc, GetStockObject(WHITE_PEN as _));
             // make sure it's round
             let height = rc.bottom - rc.top;
             let width = rc.right - rc.left;
-            let radius = min(height, width) / 2 - pad;
+            let radius = min(height, width) / 2 - padding;
             let centre_x = (rc.left + rc.right) / 2;
             let centre_y = (rc.top + rc.bottom) / 2;
             let bounds = RECT {
@@ -85,26 +82,6 @@ impl PitchPainter {
                     true,
                 );
             }
-
-            // FillRect(hdc, rc, self.background as _);
-            // FrameRect(hdc, rc, self.border as _);
-
-            // SelectObject(hdc, self.pen as _);
-            // SelectObject(hdc, self.yellow as _);
-            // Ellipse(hdc, rc.left + 20, rc.top + 20, rc.right - 20, rc.bottom - 20);
-
-            // SelectObject(hdc, self.white as _);
-            // Ellipse(hdc, 60, 60, 130, 130);
-            // Ellipse(hdc, 150, 60, 220, 130);
-
-            // SelectObject(hdc, self.black as _);
-
-            // Ellipse(hdc, 80, 80, 110, 110);
-            // Ellipse(hdc, 170, 80, 200, 110);
-
-            // SelectObject(hdc, self.red as _);
-            // let pts = &[P { x: 60, y: 150 }, P { x: 220, y: 150 }, P { x: 140, y: 220 }];
-            // Polygon(hdc, pts.as_ptr(), pts.len() as _);
         }
 
         paint.end_paint(&ps);
@@ -158,7 +135,6 @@ impl PitchPainter {
                     max_radial_intercept_y as i32,
                 );
             }
-            // LineTo(hdc, nXEnd, nYEnd);
         }
     }
 }
